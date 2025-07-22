c76ecfb974f46a1afbec25ce5c2170ed76d484a5
karrothu vamsi krishna <karrothuvamsikrishna2@gmail.com>
2025-07-22 10:28:38 +0530

    added new file

---
+++ localrepo/vamsi.py
@@ -0,0 +1,26 @@
+print("ALTROZ SUPER MARKET")
+for i in range(2):
+    print("---------------------------------------")
+print("ADDRESS: visakhapatnam,andhra pradesh,india")
+for i in range (1):
+    print("=======================================")
+    input("CUSTOMER NAME : ")
+for i in range (1):
+    print("                                       ")
+print("ITEMS:")
+ITEM_NAME = input("ITEM NAME:")
+QUANTITY = int(input("QUANTITY:"))
+PRICE = float(input("PRICE:"))
+MARKET_PRICE = QUANTITY * PRICE
+print(f"MARKET PRICE = {MARKET_PRICE}")
+DISCOUNT = MARKET_PRICE*40/100
+print(f"DISCOUNT = {DISCOUNT}")
+TOTAL_PRICE = MARKET_PRICE-DISCOUNT
+for i in range(1):
+   print("---------------------------------------")
+print(f"TOTAL PRICE : {TOTAL_PRICE} ONLY/-")
+for i in range(1):
+    print("======================================")
+    print("**************THANKS FOR SHOPPING**************")
+    
+    
